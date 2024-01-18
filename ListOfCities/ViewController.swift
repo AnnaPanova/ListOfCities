c30b34caa80b2043c01fe9d3efe0ee1c72c13161
Anna Panova <annapanovadev@gmail.com>
2024-01-18 20:11:20 +0100

    Initial Commit

---
+++ ListOfCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListOfCities
+//
+//  Created by Anna Panova on 18.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
